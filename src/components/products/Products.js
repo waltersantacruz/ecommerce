8ff0bfc2e4bcc93792a12c086c7d102b2ce6b9d3
Walter <walter.santacruz@correounivalle.edu.co>
2021-07-26 13:34:08 -0500

    arreglé error de renderizado del grid

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './product/Product'
-import jacket from '../../assets/jacket.jpg'
-import shoe from '../../assets/shoe.jpg'
 import useStyles from './styles.js'
 
-/*const products = [
-    { id: 1, name:'Shoes', description: 'Running Shoes', price: '$300', image: {shoe}},
-    { id: 2, name:'Jacket', description: 'Jacket', price: '$120', image: {jacket}}
-]*/
 
 const Products = ({products}) => {
     const classes = useStyles();
     return( 
         <main className={classes.content}> 
             <div classname={classes.toolbar}/>
-            <Grid containter spacing={4} justify="center" >
+            <Grid container spacing={4} justify="center" >
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <div className={classes.item} />
